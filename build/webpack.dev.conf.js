27c45c87a3972160e1210ee3fa30272dc8987cd5
sxding <sx380449900@gmail.com>
2018-06-20 14:09:41 +0800

    update

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -9,8 +9,11 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
-
+const Package = require('../package.json');
 const HOST = process.env.HOST
+if(Package.port) {
+  process.env.PORT = Package.port
+}
 const PORT = process.env.PORT && Number(process.env.PORT)
 
 const devWebpackConfig = merge(baseWebpackConfig, {
